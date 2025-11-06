8600627034842ed6d8e6f15a0174d16bc6b79389
ParthibanKanini <Parthiban.Chinnakkannan@kanini.com>
2025-11-06 20:56:15 +0530

    Fixed the planner agent errors and seed file

--- playwright/SPAF-Test-Automation/tests/seed.spec.ts
+++ playwright/SPAF-Test-Automation/tests/seed.spec.ts
@@ -2,6 +2,7 @@ import { test } from '../fixtures/login.fixtures.js';
 
 test.describe('Seed for login user', () => {
   test('seed using login fixture', async ({ pageFactory }): Promise<void> => {
-    const pageCreator = pageFactory;
+    const homePage = pageFactory.getHomePage();
+    homePage.assertPageLoaded();
   });
 });
